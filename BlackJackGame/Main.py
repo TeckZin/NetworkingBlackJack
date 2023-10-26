65874011131568fe6259a90a88b27f0dbfeea114
TeckWIT <114758496+TeckZin@users.noreply.github.com>
2023-10-26 14:20:45 -0400

    more done

--- BlackJackGame/Main.py
+++ BlackJackGame/Main.py
@@ -1,8 +1,34 @@
 import Game
 
 game = Game.Game(4)
-game.printDeck()
-game.printAllPlayers()
+def hitOrStand(playerNumber, strike):
+    if(strike == 3):
+        print("STAND")
+        return False
+    print(f"Your cards player {playerNumber} -> ")
+    print(player.getPlayerDeck())
+    value = str(input("hit or stand: "))
+    if value.upper() == "HIT":
+        print("HIT")
+        Flag = True
+    elif value.upper() == "STAND":
+        print("STAND")
+        Flag = False
+    else:
+        print(f"Invalid Strike {strike+1}")
+        return hitOrStand(playerNumber, strike + 1)
+
+    return Flag
+
+def calculateValue(player):
+    return None
+
+
+def hit(player):
+    card = game.GenerateCard()
+    player.addCard(card)
+
+
 
 turn = 1
 
@@ -11,9 +37,15 @@ while True:
     for player in playersList:
         playerNumber = player.getPlayerNumber()
         isHouse = player.getHouseFlag()
-        print(player.getPlayerDeck())
+        hit = hitOrStand(playerNumber, 0)
+
+
+
+
 
 
 
     turn += 1
     break
+
+
