2deb5fe045517f4e78a59b91a0b2e18dc08c352b
TeckWIT <114758496+TeckZin@users.noreply.github.com>
2023-11-08 10:19:45 -0500

    reformating

--- Networking/PlayerClient.py
+++ Networking/PlayerClient.py
@@ -1,32 +1,24 @@
 import socket, pickle
 from BlackJackGame import PacketPlayer
 
-class PlayerClient:
-    def __init__(self):
-        ip = str(input("Ip Address: "))
-        port = 1234
 
-        answer = str(input("Connect: "))
-        if answer.upper() == "Y":
-            packet = PacketPlayer.PacketPlayer(True)
-            self.sentPacket(ip, port, packet)
+def sentPacket(self, host, port, packet):
+    clientSocketFile = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocketFile.connect((host, port))
 
+    dataString = pickle.dumps(packet)
+    clientSocketFile.send(dataString)
 
-    def sentPacket(self, host, port, packet):
-        clientSocketFile = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocketFile.connect((host, port))
+    print(clientSocketFile.recv(50))
 
-        dataString = pickle.dumps(packet)
-        clientSocketFile.send(dataString)
-
-        print(clientSocketFile.recv(50))
-
-        clientSocketFile.close()
+    clientSocketFile.close()
 
 
+# ip = str(input("Ip Address: "))
+ip = socket.gethostname()
+port = 1234
 
-
-
-
-
-
+answer = str(input("Connect: "))
+if answer.upper() == "Y":
+    packet = PacketPlayer.PacketPlayer(True)
+    self.sentPacket(ip, port, packet)
